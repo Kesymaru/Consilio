b7a74e98bd6c8352e51deb7bb833b7ff90e5b8c6
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-12-07 19:34:23 -0600

    editro ESTABLE
    incluido un editro para facilitar la edicion del contenido

---
+++ editor/config.js
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	//config.language = 'es';
+	config.uiColor = '#ffffff';
+	// 
+	config.removeButtons = 'Form,Flash,Find,Replace,NewPage,Save,New_Page,Anchor,Image,About,Source,Iframe,ShowBlocks,Checkbox,Radio,TextField,Textarea,SelectionField,select,SelectAll,Button,ImageButton,HiddenField';
+};
