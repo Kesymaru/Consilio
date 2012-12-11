0fd2fa05c723ff72e7dd0871b597ab8d1181b8e7
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-12-10 19:17:48 -0600

    cofiguracion estable

--- editor/config.js
+++ editor/config.js
@@ -4,9 +4,10 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	//config.language = 'es';
-	config.uiColor = '#ffffff';
+	// Define changes to default configuration here.
+
+	config.language = 'es';
+	config.uiColor = '#FFFFFF';
 	//config.toolbarCanCollapse = true;
 	
 	// The toolbar groups arrangement, optimized for two toolbar rows.
@@ -22,10 +23,10 @@ CKEDITOR.editorConfig = function( config ) {
 		//'/',
 		{ name: 'paragraph'},
 		{ name: 'styles' },
-		//{ name: 'others' },
+		{ name: 'others' },
 		{ name: 'tools' },
 	];
 
-	config.removeButtons = 'Cut,Copy,Paste,Form,Flash,Find,Replace,,Save,New_Page,Anchor,Image,About,Source,Iframe,ShowBlocks,Checkbox,Radio,TextField,Textarea,SelectionField,select,SelectAll,Button,ImageButton,HiddenField';
+	config.removeButtons = 'Cut,Copy,Paste,Form,Flash,Find,Replace,Save,New_Page,Anchor,Image,About,Source,Iframe,ShowBlocks,Checkbox,Radio,TextField,Textarea,SelectionField,SelectAll,Button,ImageButton,HiddenField';
 
 };
