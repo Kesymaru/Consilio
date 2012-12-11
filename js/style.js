c959484734daec12c4fc3275bdd9589c50f567d5
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-12-11 11:36:10 -0600

    ajusta el tamano en ie y opera

--- js/style.js
+++ js/style.js
@@ -5,22 +5,25 @@
 
 $(window).load(function() {
 	if($.browser.opera || $.browser.msie ){
-    	var body = $('body').height();
-    	$('.menu').css('height', (body*0.8));
+    	/*var body = $('body').height();
+    	$('.menu').css('min-height', (body*0.8));
     	$('.content').css({
 	    		'height' : (body*0.8),
 	    		'overflow' : "auto"
 	    	});
-    	$('.super').css('height', (body*0.1));
+    	$('.super').css('height', (body*0.1));*/
     }
 
     $(window).resize(function() {
 
 		if($.browser.opera || $.browser.msie ){
 			var body = $('body').height();
-	    	$('#menu').css('height', (body*0.8));
-	    	//$('#content').css('height', (body*0.8));
-	    	//$('.super').css('height', (body*0.1));
+	    	$('.menu').css('min-height', (body*0.8));
+	    	$('.content').css({
+		    		'height' : (body*0.8),
+		    		'overflow' : "auto"
+		    	});
+	    	$('.super').css('height', (body*0.1));
 		}
 
 	});
