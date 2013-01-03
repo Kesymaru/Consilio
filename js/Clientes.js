053c1bc2ae28229b5b3f0a2b48da15ca7a7e460c
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-01-03 17:52:09 -0600

    Clientes.js y Edicion.js para session de clientes y edicion respectivamente, desarrollo

---
+++ js/Clientes.js
@@ -0,0 +1,15 @@
+/**
+* JAVASCRIPT PARA VISTA DE CLIENTES
+*/
+
+/**
+* CARGA LA EDICION DE CLIENTES
+*/
+function EditarClientes(){
+	if($("#menu2").is(":visible")){
+		Menu2();
+	}
+
+	LimpiarContent();
+}
+
