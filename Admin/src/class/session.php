85172e39f5a5631b775b622b1c12df39e8309545
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-01-31 11:17:28 -0600

    test

--- Admin/src/class/session.php
+++ Admin/src/class/session.php
@@ -217,10 +217,10 @@ class SessionInvitado{
 
 		$path = session_save_path();
 		
-		if (!is_writable(session_save_path()) {
+		if (!is_writable(session_save_path()) ) {
     		echo 'Session path "'.session_save_path().'" is not writable for PHP!'; 
 		}
-		
+
 		echo $path;
 		session_start();
 
