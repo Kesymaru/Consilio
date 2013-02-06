d710c543e9c613138231c308d7bf769ee59031fa
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-02-06 17:47:47 -0600

    test login remote

--- remotelogin.php
+++ remotelogin.php
@@ -8,9 +8,10 @@ require_once("src/class/session.php");
 
 if( isset($_POST['username']) && $_POST['password'] ){
 	echo 'logueando';
+	
 	$session = new Session();
-	if ( $session->RemoteLogIn($_POST['username'], $_POST['password']) ){
-		echo $_POST['home'];
+	if( $session->RemoteLogIn($_POST['username'], $_POST['password']) ){
+		echo $_SESSION['home'];
 	}else{
 		echo 'Error: credenciales invalidas.';
 	}
