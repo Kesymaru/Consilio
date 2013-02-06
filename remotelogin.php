801b2414c6d38bf83d4c7b02fa426bb75df75a1e
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-02-06 17:44:39 -0600

    posibe fix para session remotelogin

--- remotelogin.php
+++ remotelogin.php
@@ -8,7 +8,7 @@ require_once("src/class/session.php");
 
 if( isset($_POST['username']) && $_POST['password'] ){
 	$session = new Session();
-	if ( $session->RemoteLogIn($_POST['usuario'], $_POST['password']) ){
+	if ( $session->RemoteLogIn($_POST['username'], $_POST['password']) ){
 		echo $_POST['home'];
 	}else{
 		echo 'Error: credenciales invalidas.';
