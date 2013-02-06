b995a907eff2336931ade4eaf1019defef9ac724
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-02-06 17:46:30 -0600

    test remotelogin

--- remotelogin.php
+++ remotelogin.php
@@ -7,6 +7,7 @@
 require_once("src/class/session.php");
 
 if( isset($_POST['username']) && $_POST['password'] ){
+	echo 'logueando';
 	$session = new Session();
 	if ( $session->RemoteLogIn($_POST['username'], $_POST['password']) ){
 		echo $_POST['home'];
