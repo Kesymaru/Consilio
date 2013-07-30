d166aa5c61bf23ad4f623c3905a652c992b0066f
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-07-29 18:26:24 -0600

    ajax para las actualizaciones

--- Admin/src/ajaxActualizaciones.php
+++ Admin/src/ajaxActualizaciones.php
@@ -2,6 +2,7 @@
 
 require_once("class/actualizaciones.php");
 require_once("class/proyectos.php");
+require_once("class/config.php");
 
 if( isset($_POST['func']) ){
 	switch( $_POST['func'] ){
@@ -11,10 +12,17 @@ if( isset($_POST['func']) ){
 			echo Proyectos();
 			break;
 
-		//
+		//ACTUALIZACIONES DE UN PROYECTO
 		case 'Actualizaciones':
-			if( isset($_POST['proyectos']) ){
+			if( isset($_POST['proyecto']) ){
 				echo Actualizaciones($_POST['proyecto']);
+			}
+			break;
+
+		//EDICION DE UNA ACTUALIZACION
+		case 'editarActualizacion':
+			if( isset($_POST['proyecto']) && isset($_POST['id'])){
+				echo EditarActualizacion( $_POST['proyecto'], $_POST['id'] );
 			}
 			break;
 
@@ -26,6 +34,7 @@ if( isset($_POST['func']) ){
  * @return string $lista;
  */
 function Proyectos(){
+	$proyectos = new Proyectos();
 	$actualizaciones = new Actualizaciones();
 
 	$lista = '<div class="titulo" >
@@ -33,11 +42,14 @@ function Proyectos(){
 	          </div>
 	          <ul id="proyectos-actualizaciones">';
 
-	if( $proyectos = $actualizaciones->getProyectos() ){
+	if( $datos = $proyectos->getProyectosActualizaciones() ){
 
-		foreach( $proyectos as $key => $proyecto ){
+		foreach( $datos as $key => $proyecto ){
+			$contador = $actualizaciones->getContadorProyecto($proyecto['id']);
+
 			$lista .= '<li id="'.$proyecto['id'].'">
 						'.$proyecto['nombre'].'
+						<span class="contador">'.$contador.'</span>
 					   </li>';
 		}
 	}else{
@@ -57,39 +69,91 @@ function Proyectos(){
  * @return string lista html de la lista
  */
 function Actualizaciones( $proyecto ){
-	$actualizaciones = new Actualizaciones();
 	$proyectos = new Proyectos();
+	$actualizaciones = new Actualizaciones();
 
-	$proyecto = $proyectos->getProyectoDato("nombre", $proyecto);
+	$proyectoName = $proyectos->getProyectoDato("nombre", $proyecto);
 
 	$lista = '<div class="titulo">
-				'.$proyecto.' Actualizaciones
+				'.$proyectoName.'
 		      </div>
-		      <ul>';
+		      <div id="actualizaciones">
+		        <ul>';
 
 	if( $datos = $actualizaciones->getActualizaciones($proyecto) ){
 
 		foreach($datos as $key => $actualizacion){
+
 			$lista .= '<li id="'.$actualizacion['id'].'" >
 							'.$actualizacion['nombre'].'
 					   </li>';
 		}
+
 	}else{
 		$lista .= '<li class="nodata"><span>+</span></li>';
 	}
 
-	$lista .= '</ul>';
+	$lista .= '</div>q
+			   </ul>';
 
 	return $lista;
 }
 
 /**
  * EDITAR ACTUALIZACION
+ * @param int $proyecto
  * @param int $id
  * @return boolean
  */
-function EditarActualizacion( $id ){
+function EditarActualizacion( $proyecto, $id ){
+	$actualizaciones = new Actualizaciones();
 
+	$nombre = $actualizaciones->getDato("nombre", $id);
+
+	$form = '<div class="titulo">
+	            Edicion '.$nombre.'
+	         </div>
+	         <div class="datos">';
+
+	if( $datos = $actualizaciones->getActualizacion($id) ){
+		$form .= '<form method="POST" action="src/ajaxActualizaciones.php">
+					<table>
+						<tr>
+							<td>
+								Nombre
+							</td>
+							<td>
+								<input type="text" name="nombre" class="" placeholder="nombre" value="'.$datos[0]['nombre'].'">
+							</td>
+						</tr>
+						<tr>
+							<td title="Fecha en que inicia la vigencia">
+								Fecha Vigencia
+							</td>
+							<td>
+								<input type="text" name="fecha" value="'.$datos[0]['fecha_vigencia'].'">
+							</td>
+						</tr>
+					</table>
+					Notas<br/>
+					<textarea>'.$datos[0]['notas'].'</textarea>
+	            </form>';
+
+	}else{
+		$form .= '<p>No hay datos</p>';
+	}
+
+	$form .= '</div>
+			  <div class="adjuntos">
+			  </div>';
+
+	$form .= '<div class="datos-botones">
+	            <input type="submit" value="Guardar">
+	            <input type="reset" value="Limpiar">
+	            <button type="button" class="button-cancelar">Cancelar</button>
+	          </div>';
+
+	return $form;
 }
 
 /**
