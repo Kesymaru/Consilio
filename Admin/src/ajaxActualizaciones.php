08597dd5e6c6460b68671100686e4901e4df52d3
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2013-07-31 11:51:25 -0600

    Formularios para crear y editar atualizaciones

--- Admin/src/ajaxActualizaciones.php
+++ Admin/src/ajaxActualizaciones.php
@@ -19,10 +19,38 @@ if( isset($_POST['func']) ){
 			}
 			break;
 
+		//EDICION DE NUEVA ACTUALIZACION
+		case 'NuevaActualizacion':
+			if( isset($_POST['proyecto']) ){
+				echo NuevaActualizacion( $_POST['proyecto'] );
+			}
+			break;
+
+		//NUEVA ACTUALIZACION
+		case 'CrearActualizacion':
+			if( isset($_POST['proyecto']) && isset($_POST['nombre']) && isset($_POST['notas']) && isset($_POST['fecha']) ){
+				CrearActualizacion();
+			}
+			break;
+
 		//EDICION DE UNA ACTUALIZACION
-		case 'editarActualizacion':
+		case 'EditarActualizacion':
 			if( isset($_POST['proyecto']) && isset($_POST['id'])){
 				echo EditarActualizacion( $_POST['proyecto'], $_POST['id'] );
+			}
+			break;
+
+		//ACTUALIZAR ACTUALIZACION EDITADA
+		case 'ActualizarActualizacion':
+			if( isset($_POST['id']) && isset($_POST['nombre']) && isset($_POST['fecha']) ){
+				ActualizarActualizacion($_POST['id']);
+			}
+			break;
+
+		//ELIMINAR ACTUALIZACION
+		case "EliminarActualizacion":
+			if( isset($_POST['id']) ){
+				EliminarActualizacion($_POST['id']);
 			}
 			break;
 
@@ -39,7 +67,13 @@ function Proyectos(){
 
 	$lista = '<div class="titulo" >
 	            Proyectos
-	          </div>
+	          <img class="boton-buscar icon" title="Buscar Actualizaciones" onclick="Busqueda(\'busqueda-proyectos-actualizaciones\', \'busqueda-proyectos-actualizaciones-input\', \'proyectos-actualizaciones\', false)" src="images/search2.png">
+		      </div>
+		      <div class="busqueda" id="busqueda-proyectos-actualizaciones" style="display: none;">
+					<div class="buscador">
+							<input type="search" title="Escriba Para Buscar Entidades" id="busqueda-proyectos-actualizaciones-input" placeholder="Buscar Actualizaciones">
+					</div>
+			  </div>
 	          <ul id="proyectos-actualizaciones">';
 
 	if( $datos = $proyectos->getProyectosActualizaciones() ){
@@ -76,9 +110,15 @@ function Actualizaciones( $proyecto ){
 
 	$lista = '<div class="titulo">
 				'.$proyectoName.'
+				<img class="boton-buscar icon" title="Buscar Actualizaciones" onclick="Busqueda(\'busqueda-actualizaciones\', \'busqueda-actualizaciones-input\', \'actualizaciones\', false)" src="images/search2.png">
 		      </div>
+		      <div class="busqueda" id="busqueda-actualizaciones" style="display: none;">
+					<div class="buscador">
+							<input type="search" title="Escriba Para Buscar Entidades" id="busqueda-actualizaciones-input" placeholder="Buscar Actualizaciones">
+					</div>
+			  </div>
 		      <div id="actualizaciones">
-		        <ul>';
+		        <ul class="scroll" >';
 
 	if( $datos = $actualizaciones->getActualizaciones($proyecto) ){
 
@@ -89,14 +129,85 @@ function Actualizaciones( $proyecto ){
 					   </li>';
 		}
 
-	}else{
-		$lista .= '<li class="nodata"><span>+</span></li>';
 	}
 
-	$lista .= '</div>q
-			   </ul>';
+	$lista .= '</div>
+			   </ul>
+			   <div class="menu-botones">
+				<button type="button" onclick="$Actualizaciones.crearActualizacion()">Nueva</button>
+				<button type="button" class="ocultos" onclick="$Actualizacion.editarActualizacion()">Eliminar</button>
+				<button type="button" class="ocultos">Eliminar</button>
+			   </div>';
 
 	return $lista;
+}
+
+/**
+ * EDICION DE UNA NUEVA ACTUALIZACION
+ * @param int $proyecto
+ * @return boolean
+ */
+function NuevaActualizacion( $proyecto ){
+
+	$form = '<div class="titulo">
+	            Nueva Actualizacion
+	         </div>
+	         <form method="POST" action="src/ajaxActualizaciones.php" id="FormularioNuevaActualizacion">
+	         <div class="datos">
+	            <input type="hidden" name="func" value="CrearActualizacion">
+	            <input type="hidden" name="proyecto" value="'.$proyecto.'">
+					<table>
+						<tr>
+							<td>
+								Nombre
+							</td>
+							<td>
+								<input type="text" name="nombre" class="validate[required]" placeholder="nombre" >
+							</td>
+						</tr>
+						<tr>
+							<td title="Fecha en que inicia la vigencia">
+								Fecha Vigencia
+							</td>
+							<td>
+								<input type="text" name="fecha" id="fecha"  class="validate[required]" placeholder="fecha">
+							</td>
+						</tr>
+					</table>
+
+					Notas<br/>
+					<textarea name="notas" placeholder="Notas" class="validate[optional]"></textarea>
+					<br/><br/>
+			  </div>
+			  <div class="adjuntos">
+			  </div>
+
+			  <div class="datos-botones">
+			    <button type="button" class="button-cancelar" onClick="CancelarContent()">Cancelar</button>
+	            <button type="submit">Guardar</button>
+	            <button type="reset">Limpiar</button>
+	          </div>
+
+	          </form>';
+
+	return $form;
+}
+
+/**
+ * CREA UNA NUEVA ACTUALIZACION
+ */
+function CrearActualizacion(){
+	$actualizacion = new Actualizaciones();
+
+	$proyecto = $_POST['proyecto'];
+	$nombre = $_POST['nombre'];
+	$notas = $_POST['notas'];
+	$fecha = $_POST['fecha'];
+
+	if( !$actualizacion->newActualizacion($proyecto, $nombre, $notas, $fecha) ){
+		echo "Error: no se pudo crear la actualizacion.";
+	}
+
 }
 
 /**
@@ -107,23 +218,29 @@ function Actualizaciones( $proyecto ){
  */
 function EditarActualizacion( $proyecto, $id ){
 	$actualizaciones = new Actualizaciones();
+	$config  = new Config();
 
 	$nombre = $actualizaciones->getDato("nombre", $id);
 
 	$form = '<div class="titulo">
 	            Edicion '.$nombre.'
 	         </div>
+	         <form method="POST" action="src/ajaxActualizaciones.php">
 	         <div class="datos">';
 
 	if( $datos = $actualizaciones->getActualizacion($id) ){
-		$form .= '<form method="POST" action="src/ajaxActualizaciones.php">
+
+		$fecha = $config->formatDateOut($datos[0]['fecha_vigencia']);
+
+		$form .= '  <input type="hidden" name="func" value="ActualizarActualizacion">
+					<input type="hidden" name="id" value="'.$datos[0]['id'].'">
 					<table>
 						<tr>
 							<td>
 								Nombre
 							</td>
 							<td>
-								<input type="text" name="nombre" class="" placeholder="nombre" value="'.$datos[0]['nombre'].'">
+								<input type="text" name="nombre" class="validate[required]" placeholder="nombre" value="'.$datos[0]['nombre'].'">
 							</td>
 						</tr>
 						<tr>
@@ -131,13 +248,15 @@ function EditarActualizacion( $proyecto, $id ){
 								Fecha Vigencia
 							</td>
 							<td>
-								<input type="text" name="fecha" value="'.$datos[0]['fecha_vigencia'].'">
+								<input type="text" name="fecha" id="fecha" class="validate[required]" placeholder="fecha" value="'.$fecha.'">
 							</td>
 						</tr>
 					</table>
+
 					Notas<br/>
-					<textarea>'.$datos[0]['notas'].'</textarea>
-	            </form>';
+					<textarea name="notas" class="validate[optional]" placeholder="notas">'.$datos[0]['notas'].'</textarea>
+					<br/><br/>
+	            ';
 
 	}else{
 		$form .= '<p>No hay datos</p>';
@@ -148,19 +267,48 @@ function EditarActualizacion( $proyecto, $id ){
 			  </div>';
 
 	$form .= '<div class="datos-botones">
-	            <input type="submit" value="Guardar">
-	            <input type="reset" value="Limpiar">
-	            <button type="button" class="button-cancelar">Cancelar</button>
-	          </div>';
+				<button type="button" class="button-cancelar" onClick="CancelarContent()">Cancelar</button>
+	            <button type="submit">Guardar</button>
+	            <button type="reset">Limpiar</button>
+	          </div>
+
+	          </form>';
 
 	return $form;
 }
 
 /**
- * ELIMINA UNA ACTUALIZACION
+ * ACTUALIZA UNA ACTUALIZACION EDITADA
  * @param int $id
  * @return boolean
  */
-function EliminarActualizacion( $id ){
+function ActualizarActualizacion($id){
+	$actualizaciones = new Actualizaciones();
+
+	$id = $_POST['id'];
+	$nombre = $_POST['nombre'];
+	$notas = "";
+	if( isset($_POST['notas']) ){
+		$notas = $_POST['notas'];
+	}
+	$fecha_vigencia = $_POST['fecha'];
+
+	if( $actualizaciones->updateActualizacion($id, $nombre, $notas, $fecha_vigencia) ){
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * ELIMINA UNA ACTUALIZACION
+ * @param $id
+ */
+function EliminarActualizacion($id){
+	$actualizaciones = new Actualizaciones();
+
+	if( !$actualizaciones->deleteActualizacion($id) ){
+		echo "Error: no se pudo eliminar la actualizacion.";
+	}
 
 }
